c1bf0db4ce12a917247634b356093367a8c26df2
jianlq <1427638644@qq.com>
2016-05-26 12:05:20 +0800

    nash

---
+++ NashVSstackelberg/TE_routing/Common.h
@@ -0,0 +1,31 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
+#include<math.h>
+#include<algorithm>
+#include<vector>
+#include<set>
+#include<queue>
+#include<string>
+#include<iostream>
+#include<fstream>
+#include<math.h>
+#include<list>
+#include<map>
+#include<crtdbg.h>
+using namespace std;
+
+#define NUMDEMAND 10
+#define MAXDEMAND 15
+#define INF 1000000
+#define MAXWEIGHT 5
+#define MIN 0.05
+#define CONSTANT 0
+#define K 16
+#define MINCAPACITY 30
+
+#endif
