f5185b0fe80cd1795def663688aa6c162d6bd795
jianlq <1427638644@qq.com>
2016-05-26 12:22:08 +0800

    SameObj

---
+++ TEORSameObj/TE_routing/Common.h
@@ -0,0 +1,33 @@
+#ifndef COMMON
+#define COMMON
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
+#include<math.h>
+#include<algorithm>
+#include<vector>
+#include<set>
+#include<queue>
+#include<string>
+#include<iostream>
+#include<fstream>
+#include<math.h>
+#include<list>
+#include<map>
+#include<crtdbg.h>
+using namespace std;
+
+#define NUMDEMAND 15
+//#define ORNUMDEMAND 12
+#define MAXDEMAND 15
+#define MAXWEIGHT 5
+#define INF 100000000
+#define MIN 0.5
+#define CONSTANT 1
+#define BACKGROUND 1
+#define SAMEDELAY 0
+#define SAMEMLU 1
+#define K 8
+#endif
