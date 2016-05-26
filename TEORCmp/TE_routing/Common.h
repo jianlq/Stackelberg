29fc0dddfb369b8653c0f95c4ef18430c941233f
jianlq <1427638644@qq.com>
2016-05-26 12:20:33 +0800

    TEORCmp

---
+++ TEORCmp/TE_routing/Common.h
@@ -0,0 +1,30 @@
+#ifndef COMMON
+#define COMMON
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
+#include<math.h>
+#include<algorithm>
+#include<vector>
+#include<set>
+#include<queue>
+#include<string>
+#include<iostream>
+#include<fstream>
+#include<math.h>
+#include<list>
+#include<map>
+#include<crtdbg.h>
+using namespace std;
+
+#define NUMDEMAND 15
+//#define ORNUMDEMAND 12/16/28
+#define MAXDEMAND 11
+#define MAXWEIGHT 5
+#define INF 100000000
+#define MIN 0.5
+#define CONSTANT 1
+#define K 8
+#endif
